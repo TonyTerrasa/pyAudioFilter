be1574db041334429edf299950342b40013b4290
TonyTerrasa <tony.terrasa@gmail.com>
2019-10-11 10:29:55 -0400

    first commit

---
+++ biquad_optimizer.py
@@ -0,0 +1,153 @@
+# ------------------------------------------------------------
+# Objective: match a frequency responce my moving poles and zeros
+# ------------------------------------------------------------
+
+import tensorflow as tf
+import numpy as np
+import scipy.signal as sig
+from filter_helpers import *
+
+
+#plot_sos(target, FS)
+
+MAX_POLE_NORM = 0.900
+
+class BiQuadOptimizer:
+
+    def __init__(self, learning_rate=0.1):
+        # optimizing/pushing the poles and zeros of this system
+        self.z1 = tf.Variable(tf.random.uniform(shape=(1,), dtype=tf.float64)) # first zero: pure real
+        self.z2 = tf.Variable(tf.random.uniform(shape=(1,), dtype=tf.float64)) # first zero: pure real
+        self.p  = tf.Variable(tf.random.uniform(shape=(2,), dtype=tf.float64)) # both poles (complex conj)
+        self.p.assign(tf.clip_by_norm(self.p, MAX_POLE_NORM))
+        self.g = tf.Variable(tf.random.uniform(shape=(1,), dtype=tf.float64)) # gain
+        self.train_vars = [self.z1, self.z2, self.p, self.g]
+        self.learning_rate = learning_rate
+
+    # for some reason, this breaks without the tf.function decorator
+    #@tf.function
+    def loss(self, w, mag_target):
+        # cartesian coordinates of the input signals
+        # note that we assume a norm of 1, that is a pure sinosoid
+        x_loc  = tf.cos(w)
+        y_loc  = tf.sin(w)
+
+        #print(x_loc)
+        #print(x_loc, y_loc)
+
+        # calculating the magnitude of the output using the distances to Poles
+        # and zeroes
+        dist_z1 = tf.sqrt(tf.square(x_loc - self.z1[0]) + tf.square(y_loc - 0) )
+        dist_z2 = tf.sqrt(tf.square(x_loc - self.z2[0]) + tf.square(y_loc - 0) )
+        dist_p1 = tf.sqrt(tf.square(x_loc - self.p[0])  +  tf.square(y_loc - self.p[1]))
+        dist_p2 = tf.sqrt(tf.square(x_loc - self.p[0])  +  tf.square(y_loc + self.p[1])) # complex conj
+
+        # numerator and denominator of the transfer function for these frequencies
+        # are product of ht edistances to the zeroes and poles
+        numerator = dist_z1 * dist_z2
+        denominator = dist_p1 * dist_p2
+        #print(denominator)
+
+        # magnitude in linear units and db
+        linear_magnitude = self.g * (numerator / denominator)
+
+
+        #return linear_magnitude
+
+        # note that tf.math.log is the natural log the log base 10 can be callculated
+        # as log(x)/log(10)
+        db_magnitude = 20 * tf.math.log(linear_magnitude) / tf.dtypes.cast(tf.math.log(10.0), dtype=tf.float64)
+
+        return tf.reduce_sum(tf.square(db_magnitude - mag_target))/1000
+
+
+    def train(self, w, mag_target):
+
+
+        #print(self.current_loss)
+            
+        # calculate the gradients for each variable
+        with tf.GradientTape() as t:
+
+            self.current_loss = self.loss(w, mag_target)
+            #print(self.train_vars)
+            grads = t.gradient(self.current_loss, self.train_vars)
+
+        #print(grads)
+
+        # apply the gradients to the variables
+        for i, var in enumerate(self.train_vars):
+            var.assign_sub(self.learning_rate*grads[i])
+
+        #print(grads)
+
+        #print(tf.clip_by_norm(self.p, MAX_POLE_NORM))
+        self.p.assign(tf.clip_by_norm(self.p, MAX_POLE_NORM)) # keep pole in unit cirlce
+
+    def get_zs(self):
+        return (self.z1.numpy()[0], self.z2.numpy()[0])
+
+    def get_ps(self):
+        return self.p.numpy()
+
+FS = 44100
+TARGET_FC = 2000 # cutoff frequency
+TARGET_ORDER = 2
+TARGET_TYPE = 'lowpass' # 'highpass', 'bandpass', 'bandstop'
+
+# target polar response
+# butterworth filter
+# this is returned coefficients (sos)
+target = sig.butter(TARGET_ORDER, TARGET_FC, TARGET_TYPE, analog=False, fs=FS, output='sos')
+
+
+#calc a target at some set of frequencies
+# target that we are using is the magntide in db
+# of the desired
+targ_freqs = np.array([100,300,2200,5000,10000,12500,15000,20000])
+w, mag_target = get_mag_targ(target, targ_freqs, fs=FS)
+
+
+NUM_EPOCHS = 1100
+optimizer = BiQuadOptimizer(learning_rate=0.001)
+
+losses = []
+zs = []
+ps = []
+
+for epoch in range(NUM_EPOCHS):
+
+    optimizer.train(w, mag_target)
+    losses.append(optimizer.current_loss)
+    zs.append(optimizer.get_zs())
+    ps.append(optimizer.get_ps())
+    print("loss: %f, z1: %f, z2: %f, p: %f,%f, g: %f"%(optimizer.current_loss, optimizer.z1.numpy()[0], optimizer.z2.numpy()[0], optimizer.p.numpy()[0], optimizer.p.numpy()[1], optimizer.g.numpy()[0]))
+    print('-'*25)
+
+
+# ---------------------------------------------
+# plot the results
+# ---------------------------------------------
+
+# get the final poles and zeros
+z, p = tfvar_to_zpk([optimizer.z1, optimizer.z2], [optimizer.p])
+
+# plot the target freq response
+plot_sos(target, FS, False)
+# plot the resulting freq response
+plot_zpk(z, p, optimizer.g.numpy()[0], FS, False)
+
+# 
+z1,z2 = zip(*zs)
+p_r,p_i = zip(*ps)
+
+plt.figure()
+plt.plot(losses)
+plt.plot(z1)
+plt.plot(z2)
+plt.plot(p_r)
+plt.plot(p_i)
+plt.legend(["loss", "z1", "z2", "p_{real}", "p_{image}"])
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.show()
---
+++ filter_helpers.py
@@ -0,0 +1,105 @@
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from matplotlib import patches
+from matplotlib.gridspec import GridSpec
+from matplotlib.colors import colorConverter
+import math
+
+
+def tfvar_to_zpk(zeros, poles):
+    """
+    Takes in a lists of zeros and poles as tf.Variable and converts them to
+    complex poles and zeros
+    """
+    
+    zs = [z.numpy()[0] for z in zeros]
+    ps = []
+    for p in poles:
+        ps.append(p.numpy()[0] + 1j*p.numpy()[1])
+        ps.append(p.numpy()[0] - 1j*p.numpy()[1])
+
+    return zs, ps
+
+
+def get_complex_targ(sos, freqs, fs):
+    #return db mag and phase at frequencies given an sos filter
+    return sig.sosfreqz(sos, 2.0*np.pi*np.array(freqs)/fs, fs)
+
+
+def get_mag_targ(sos, freqs, fs):
+    #return db mag at radian angles given an sos filter
+    w, h = sig.sosfreqz(sos, 2.0*np.pi*np.array(freqs)/fs, fs)
+    return w, 20*np.log10(np.abs(h))
+
+
+def octaves_to(freq, appendFs=False):
+    #get octave freqs from 20 hz to <fs, +fs
+    octspace = [20]
+
+    while octspace[-1]*2.0 <= freq: octspace.append(int(octspace[-1]*2.0))
+    if octspace[-1] != freq and appendFs: octspace.append(int(freq))
+
+    return octspace
+
+def plot_sos(sos, fs, show=True):
+
+
+    w, h = sig.sosfreqz(sos, fs)
+
+    # gets the poles and zeros of the given second order system
+    z , p, _ = sig.sos2zpk(sos)
+
+    fig = plt.figure(figsize=(20,4))
+    gs = GridSpec(2, 2, width_ratios = [1,2])
+
+    #plot poles/zeros
+    ax1 = fig.add_subplot(gs[0:,0])
+
+    #gives the unit circle
+    unit_circle = patches.Circle((0,0), radius=1, fill=False,
+                                 color='grey', ls='dotted', alpha=0.5)
+    ax1.add_patch(unit_circle)
+
+    #
+    for pole in p:
+        plt.plot(pole.real, pole.imag, 'x', markersize=7, alpha=0.7, color='blue')
+    for zero in z:
+        plt.plot(zero.real, zero.imag, 'o', markersize=7, markerfacecolor='none',
+                markeredgecolor=colorConverter.to_rgba('mediumseagreen', alpha=0.7))
+    ax1.grid(True)
+    ax1.set_xlim(-1.2,1.2)
+    ax1.set_ylim(-1.2,1.2)
+    ax1.set_title('Z domain Poles/Zeros')
+
+    #plot mag/phase
+    ax2 = fig.add_subplot(gs[0,1:])
+    db = 20*np.log10(np.abs(h))
+    ax2.plot(w/np.pi*fs/2, db)
+    ax2.set_xscale('symlog')
+    spacing = octaves_to(fs/2)
+    ax2.set_xticks(spacing)
+    ax2.set_xticklabels(spacing, rotation=30)
+    ax2.set_ylim(-75, 5)
+    ax2.grid(True)
+    ax2.set_yticks([0, -20, -40, -60])
+    ax2.set_ylabel('Gain [dB]')
+    ax2.set_title('Frequency Response')
+
+    ax3 = fig.add_subplot(gs[1,1:])
+    ax3.plot(w/np.pi*fs/2, np.angle(h))
+    ax3.set_xscale('symlog')
+    ax3.set_xticks(spacing)
+    ax3.set_xticklabels(spacing, rotation=30)
+    ax3.grid(True)
+    ax3.set_yticks([-np.pi, -0.5*np.pi, 0, 0.5*np.pi, np.pi],
+             [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
+    ax3.set_ylabel('Phase [rad]')
+    ax3.set_xlabel('Frequency (Hz)')
+
+    if show: plt.show()
+
+
+
+def plot_zpk(z, p, k, fs, show=True):
+    plot_sos(sig.zpk2sos(z,p,k), fs, show)
